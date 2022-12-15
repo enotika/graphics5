d5daa43c7ded022676852511225c898119fe1358
enotika <91068026+enotika@users.noreply.github.com>
2022-12-15 19:54:43 +0300

    Add files via upload

---
+++ release-lab5/release/moc_mainwindow.cpp
@@ -0,0 +1,268 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../laba5_2/mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+#include <QtCore/QList>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_MainWindow_t {
+    uint offsetsAndSizes[48];
+    char stringdata0[11];
+    char stringdata1[23];
+    char stringdata2[1];
+    char stringdata3[33];
+    char stringdata4[27];
+    char stringdata5[13];
+    char stringdata6[16];
+    char stringdata7[12];
+    char stringdata8[15];
+    char stringdata9[5];
+    char stringdata10[3];
+    char stringdata11[3];
+    char stringdata12[3];
+    char stringdata13[3];
+    char stringdata14[12];
+    char stringdata15[3];
+    char stringdata16[3];
+    char stringdata17[3];
+    char stringdata18[3];
+    char stringdata19[12];
+    char stringdata20[31];
+    char stringdata21[35];
+    char stringdata22[39];
+    char stringdata23[34];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_MainWindow_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 22),  // "on_actionadd_triggered"
+        QT_MOC_LITERAL(34, 0),  // ""
+        QT_MOC_LITERAL(35, 32),  // "on_actionadd_Rectangle_triggered"
+        QT_MOC_LITERAL(68, 26),  // "on_actioncut_off_triggered"
+        QT_MOC_LITERAL(95, 12),  // "suthHodgClip"
+        QT_MOC_LITERAL(108, 15),  // "QList<QPointF>&"
+        QT_MOC_LITERAL(124, 11),  // "poly_points"
+        QT_MOC_LITERAL(136, 14),  // "clipper_points"
+        QT_MOC_LITERAL(151, 4),  // "clip"
+        QT_MOC_LITERAL(156, 2),  // "x1"
+        QT_MOC_LITERAL(159, 2),  // "y1"
+        QT_MOC_LITERAL(162, 2),  // "x2"
+        QT_MOC_LITERAL(165, 2),  // "y2"
+        QT_MOC_LITERAL(168, 11),  // "y_intersect"
+        QT_MOC_LITERAL(180, 2),  // "x3"
+        QT_MOC_LITERAL(183, 2),  // "y3"
+        QT_MOC_LITERAL(186, 2),  // "x4"
+        QT_MOC_LITERAL(189, 2),  // "y4"
+        QT_MOC_LITERAL(192, 11),  // "x_intersect"
+        QT_MOC_LITERAL(204, 30),  // "on_actionadd_polygon_triggered"
+        QT_MOC_LITERAL(235, 34),  // "on_actionpolygon_cut_off_trig..."
+        QT_MOC_LITERAL(270, 38),  // "on_actionadd_cutting_polynom_..."
+        QT_MOC_LITERAL(309, 33)   // "on_actionclear_polygons_trigg..."
+    },
+    "MainWindow",
+    "on_actionadd_triggered",
+    "",
+    "on_actionadd_Rectangle_triggered",
+    "on_actioncut_off_triggered",
+    "suthHodgClip",
+    "QList<QPointF>&",
+    "poly_points",
+    "clipper_points",
+    "clip",
+    "x1",
+    "y1",
+    "x2",
+    "y2",
+    "y_intersect",
+    "x3",
+    "y3",
+    "x4",
+    "y4",
+    "x_intersect",
+    "on_actionadd_polygon_triggered",
+    "on_actionpolygon_cut_off_triggered",
+    "on_actionadd_cutting_polynom_triggered",
+    "on_actionclear_polygons_triggered"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      11,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   80,    2, 0x08,    1 /* Private */,
+       3,    0,   81,    2, 0x08,    2 /* Private */,
+       4,    0,   82,    2, 0x08,    3 /* Private */,
+       5,    2,   83,    2, 0x08,    4 /* Private */,
+       9,    5,   88,    2, 0x08,    7 /* Private */,
+      14,    8,   99,    2, 0x08,   13 /* Private */,
+      19,    8,  116,    2, 0x08,   22 /* Private */,
+      20,    0,  133,    2, 0x08,   31 /* Private */,
+      21,    0,  134,    2, 0x08,   32 /* Private */,
+      22,    0,  135,    2, 0x08,   33 /* Private */,
+      23,    0,  136,    2, 0x08,   34 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 6, 0x80000000 | 6,    7,    8,
+    QMetaType::Void, 0x80000000 | 6, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double,    7,   10,   11,   12,   13,
+    QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double,   10,   11,   12,   13,   15,   16,   17,   18,
+    QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double,   10,   11,   12,   13,   15,   16,   17,   18,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSizes,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_actionadd_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionadd_Rectangle_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actioncut_off_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'suthHodgClip'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QVector<QPointF> &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QVector<QPointF> &, std::false_type>,
+        // method 'clip'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QVector<QPointF> &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        // method 'y_intersect'
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        // method 'x_intersect'
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        // method 'on_actionadd_polygon_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionpolygon_cut_off_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionadd_cutting_polynom_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionclear_polygons_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_actionadd_triggered(); break;
+        case 1: _t->on_actionadd_Rectangle_triggered(); break;
+        case 2: _t->on_actioncut_off_triggered(); break;
+        case 3: _t->suthHodgClip((*reinterpret_cast< std::add_pointer_t<QList<QPointF>&>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<QList<QPointF>&>>(_a[2]))); break;
+        case 4: _t->clip((*reinterpret_cast< std::add_pointer_t<QList<QPointF>&>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[3])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[4])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[5]))); break;
+        case 5: { double _r = _t->y_intersect((*reinterpret_cast< std::add_pointer_t<double>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[3])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[4])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[5])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[6])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[7])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[8])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 6: { double _r = _t->x_intersect((*reinterpret_cast< std::add_pointer_t<double>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[3])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[4])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[5])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[6])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[7])),(*reinterpret_cast< std::add_pointer_t<double>>(_a[8])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 7: _t->on_actionadd_polygon_triggered(); break;
+        case 8: _t->on_actionpolygon_cut_off_triggered(); break;
+        case 9: _t->on_actionadd_cutting_polynom_triggered(); break;
+        case 10: _t->on_actionclear_polygons_triggered(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 11)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 11;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 11)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 11;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
